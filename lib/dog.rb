5b62214c7a4dd68b714f208967a4b5b21561b91e
kellygalka <kelly3000a1@gmail.com>
2018-08-10 14:41:54 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,5 +2,12 @@
 class Dog 
   def initialize(name,breed)
     @name=name 
+    @breed=mutt
   end 
-  def 
+  def name= new_name
+    @name =new_name 
+  end
+  def breed=new_breed
+    @breed=new_breed
+  end
+end
